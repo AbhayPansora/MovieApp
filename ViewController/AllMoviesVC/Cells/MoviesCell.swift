ae17e45c6642a1dfc14cc2c437f7f51f6fd2e597
Abhay pansora <pansoraabhay07@gmail.com>
2023-03-09 21:08:32 +0530

    first commit

---
+++ ViewController/AllMoviesVC/Cells/MoviesCell.swift
@@ -0,0 +1,23 @@
+//
+//  MoviesCell.swift
+//  MoviesApp
+//
+//  Created by Abhay Pansora on 18/02/23.
+//
+
+import UIKit
+
+class MoviesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ViewController/HomeVc/Cells/ViewAllCell.swift
@@ -0,0 +1,17 @@
+//
+//  ViewAllCell.swift
+//  MoviesApp
+//
+//  Created by Abhay Pansora on 21/02/23.
+//
+
+import UIKit
+
+class ViewAllCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
